9f1ccd67c62a779c95bb882379d736ac2166a36a
Jordon Terry <jt5493@hotmail.com>
2020-04-26 02:20:12 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -14,11 +14,20 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT species, COUNT(*) 
+  FROM characters
+  GROUP BY species
+  ORDER BY COUNT(species) DESC
+  LIMIT 1"
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT authors.name, subgenres.name 
+    FROM authors 
+    JOIN series 
+    ON series.author_id = authors.id 
+    JOIN subgenres 
+    ON series.subgenre_id = subgenres.id"
 end
 
 def select_series_title_with_most_human_characters
